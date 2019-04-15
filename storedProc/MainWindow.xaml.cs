beacd1cd3b098885aed5af1a2c3a6992055dbe4e
work <work@TEST>
2019-04-15 17:00:07 +0300

    stop centr

--- storedProc/MainWindow.xaml.cs
+++ storedProc/MainWindow.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using storedProc.Entities;
+using storedProc.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +26,39 @@ namespace storedProc
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static EFContext _context;
+        public static List<UserViewModel> userList;
         public MainWindow()
         {
             InitializeComponent();
+            _context = new EFContext();
+        }
+
+        static void GetDataSQLStorage()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var jsonOut = new SqlParameter();
+            jsonOut.ParameterName = "@JSONOutput";
+            jsonOut.Direction = ParameterDirection.Output;
+            jsonOut.SqlDbType = SqlDbType.NVarChar;
+            jsonOut.Size = -1;
+            string sql = @"EXEC [dbo].[selectUsers] @From, @To, @JSONOutput";
+            _context.Database.ExecuteSqlCommand(sql,
+                new SqlParameter("@From", Convert.ToInt32(5002)),
+                new SqlParameter("@To", Convert.ToInt32(7001)),
+                jsonOut);
+            userList = JsonConvert.DeserializeObject<List<UserViewModel>>(jsonOut.Value.ToString());
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            string elTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            MessageBox.Show(elTime);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            GetDataSQLStorage();
         }
 
         private void BtnFind_Click(object sender, RoutedEventArgs e)
